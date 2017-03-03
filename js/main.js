d8e50790d49143d2ac2fd29b373726d000123c8a
LaurenPR <LaurenPR@vagvlan567.01606.wlan.wireless-pennnet.upenn.edu>
2017-03-03 12:07:08 -0500

    going back to a working version where the slides change
    (I should do these commits more often!!)

--- js/main.js
+++ js/main.js
@@ -1,18 +1,151 @@
+/*
+Resources:
+- for leaflet: http://leafletjs.com/reference.html#map-set-methods
+- for spatial join: http://turfjs.org/ (& https://github.com/turf-junkyard/turf-tag)
+
+*/
+
 /* =====================
-Leaflet Configuration
+  Leaflet Configuration
 ===================== */
 
-var map = L.map('map', {
-  center: [40.000, -75.1090],
-  zoom: 11
-});
-var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  subdomains: 'abcd',
-  minZoom: 0,
-  maxZoom: 20,
-  ext: 'png'
-}).addTo(map);
+// var map = L.map('map', {
+//   center: [40.000, -75.1090],
+//   zoom: 11
+// });
+// var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+//   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//   subdomains: 'abcd',
+//   minZoom: 0,
+//   maxZoom: 20,
+//   ext: 'png'
+// }).addTo(map);
 
 
 /* ===================== */
+
+
+/* =====================
+  Functions (to call later)
+===================== */
+
+// This switches what is being active on the page to the next Slide (no wrapping: hard stop at end slide)
+var nextSlide = function () {
+ var limit = appState.slideInformation.length -1;
+ if  (appState.slideNumber +1 > limit) {
+   appState.slideNumber = appState.slideNumber = limit;
+ } else {appState.slideNumber = appState.slideNumber +1;
+ } return appState.slideInformation[appState.slideNumber];
+};
+
+// This switches what is being active on the page to the previous Slide (no wrapping: hard stop at first slide)
+var previousSlide = function () {
+  if  (appState.slideNumber - 1 < 0) {
+    appState.slideNumber = appState.slideNumber = 0;
+  } else {appState.slideNumber = appState.slideNumber -1;
+  }
+  return appState.slideInformation[appState.slideNumber];
+};
+
+
+
+/* =====================
+  Working with Data
+===================== */
+
+
+// Specifying what will be on each slide:
+var slide1 = {
+  'color': "#7D26CD", //purple
+  'title-Header': "My First Page!",
+  'sidebar-Text': "My text here.....",
+  // 'PanningParams': ,
+  // 'Zoomingparams': ,
+  };
+
+var slide2 = {
+  'color': "#6495ED", //blue
+  'title-Header': "My second Page!",
+  'sidebar-Text': "More text here.....",
+  // 'PanningParams': ,
+  // 'Zoomingparams': ,
+  };
+
+var slide3 = {
+  'color': "#666600",
+  'title-Header': "My third Page!",
+  'sidebar-Text': "this could talk about a filter...",
+  // 'PanningParams': ,
+  // 'Zoomingparams': ,
+  };
+
+var slide4 = {
+  'color': "#006600",
+  'title-Header': "My 4th Page!",
+  'sidebar-Text': "this could talk about a second filter option...",
+  // 'PanningParams': ,
+  // 'Zoomingparams': ,
+  };
+
+var slide5 = {
+  'color': "#660066",
+  'title-Header': "5th Page!",
+  'sidebar-Text': "this could zoom in!",
+  // 'PanningParams': ,
+  // 'Zoomingparams': ,
+  };
+
+var allSlides = [slide1, slide2, slide3, slide4, slide5];
+
+// Importing Data:
+// var geoData = LI_OUTDOOR_ADVERTISING.geojson; //hardcoding option
+// var parsedData = JSON.parse(geoData);
+// console.log(parsedData);
+
+// $ajax('').done(function(){ //active data request option
+  // return appState.parsedData; //this needs work
+
+
+
+// Defining an App State, including the current slide #, slide information, and data:
+var appState = {
+  'slideNumber': 0, // slideNumber keeps track of what slide you are on. It should increase when you
+                    // click the next button and decrease when you click the previous button. It
+                    // should never get so large that it is bigger than the dataset. It should never
+                    // get so small that it is smaller than 0.
+  'slideInformation': allSlides,
+  // 'data': parsedData,
+ };
+
+
+/* =====================
+  Executing Code
+===================== */
+
+// var setSideBarText = function () {
+// };
+
+var setColorStyle = function (slideObject) {
+   $('#blankSpace').css('background-color', slideObject.color);
+   console.log("You are on slide" + " " + appState.slideNumber);
+};
+
+// "text-heading"
+// "text-description"
+
+$("#button-next").click(function() {
+  setColorStyle(nextSlide());
+  // console.log(1, appState.slideInformation);
+  // console.log(2, appState.slideNumber);
+  // console.log(3, appState.slideInformation[appState.slideNumber]);
+  $("#text-heading").text(appState.slideInformation[appState.slideNumber]["title-Header"]);
+  $("#text-description").text(appState.slideInformation[appState.slideNumber]["sidebar-Text"]);
+
+});
+
+$("#button-previous").click(function() {
+  setColorStyle(previousSlide());
+  $("#text-heading").text(appState.slideInformation[appState.slideNumber]["title-Header"]);
+  $("#text-description").text(appState.slideInformation[appState.slideNumber]["sidebar-Text"]);
+
+});
---
+++ notesFromClass.js
@@ -0,0 +1,56 @@
+// var apple = {'color': 'red', 'name':'apple'}
+// var banana = {'color': 'yellow', 'name':'banana'}
+// var pear = {'color': 'green', 'name':'pear'}
+//
+//
+// var state = {
+//   count: 0,
+//   data:undefined,
+//  };
+//
+//
+// var nextFruit = function() {
+//   var limit = state.data.length -1;
+//   if (state.count +1 <limit) {
+//   state.count = 0;
+//   } else {
+//   return state.count = state.count +1
+//   }
+//   return state.data[state.count]
+//  }
+//
+//  // returns previous fruit (this WRAPS around!!)
+// var prevFruit = fucntion() {
+//   if (state.count -1 <0) {
+//   stae.count = state.data.length -1;
+//   } else {
+//   state.count - stae.count -1;
+//   }
+//   return state.data[state.count];
+//   };
+//
+//
+//   // converts plain text color to hex color
+//   var changeColor = function(fruitObject) {
+//    if (fruitObject.color === "yellow"}{
+//    return "#ffc125";
+      // }
+//    else if (fruitObject.color === "red"){
+//    return "#fEE4000;
+//    } else (fruitObject.color === "green") {
+//    return "#4eee94";
+//    };
+//   };
+//
+// var setStyle = function (fruitobject) {
+//    $('body').css('background-color', getHecColor())
+// }
+//
+// $ajax('').done(function(){
+// var parsedData - JSON.parse(Data);
+// state.data;
+//
+// $("#button-next").click(function(ev) {
+// setstyle(nextFruit());}
+// };
+//
